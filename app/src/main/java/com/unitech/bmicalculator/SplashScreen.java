c27dedf46fe6e609cd54250e2c07b883e6eca631
EzekielBanda <bsc-com-10-19@unima.ac.mw>
2023-02-22 10:31:49 +0200

    splash Screen implemented

---
+++ app/src/main/java/com/unitech/bmicalculator/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.unitech.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            finish();
+        }, 3000);
+    }
+}
